dd13b8e82f9bfa3340fc7c7adbf8c406e2ea94df
Lx <1020322274@qq.com>
2021-02-23 13:30:27 +0800

    first commit

---
+++ AdmireHidEnd/src/main/java/com/admirehid/AdmireHidApplication.java
@@ -0,0 +1,13 @@
+package com.admirehid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmireHidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmireHidApplication.class, args);
+    }
+
+}
