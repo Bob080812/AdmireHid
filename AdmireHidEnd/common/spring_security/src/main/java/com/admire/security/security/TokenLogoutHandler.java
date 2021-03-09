824b47bf45ed0a9f483293596114e0cac1ca0307
Lx <1020322274@qq.com>
2021-03-09 17:39:53 +0800

    spring security配置

---
+++ AdmireHidEnd/common/spring_security/src/main/java/com/admire/security/security/TokenLogoutHandler.java
@@ -0,0 +1,40 @@
+package com.admire.security.security;
+
+import com.admire.utils.utils.R;
+import com.admire.utils.utils.ResponseUtil;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 退出处理器
+ */
+public class TokenLogoutHandler implements LogoutHandler {
+
+    private TokenManager tokenManager;
+    private RedisTemplate redisTemplate;
+
+    public TokenLogoutHandler(TokenManager tokenManager,RedisTemplate redisTemplate){
+        this.tokenManager = tokenManager;
+        this.redisTemplate = redisTemplate;
+    }
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        //1.从header获取token
+        String token = request.getHeader("token");
+        //2.token不为空，移除token，删除redis中token
+        if (token != null){
+            //移除token
+            tokenManager.removeToken(token);
+            //从token获取用户名
+            String username = tokenManager.getUserInfoFromToken(token);
+            //从redis中删除
+            redisTemplate.delete(username);
+        }
+        ResponseUtil.out(response, R.ok());
+    }
+}
---
+++ AdmireHidEnd/service_expand/Permissions/src/main/java/com/admire/service/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.admire.service.service.impl;
+
+
+import com.admire.service.entity.UserRole;
+import com.admire.service.mapper.UserRoleMapper;
+import com.admire.service.service.UserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
